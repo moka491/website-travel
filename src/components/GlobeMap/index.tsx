e88790a1d86f29b519064f6a9da96a9a10850db5
Jannis Becker <31903525+moka491@users.noreply.github.com>
2022-08-31 18:09:07 +0200

    add basic lod system

--- src/components/GlobeMap/index.tsx
+++ src/components/GlobeMap/index.tsx
@@ -1,3 +1,4 @@
+import { Detailed } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
 import CameraControls from "./CameraControls";
 import Globe from "./Globe";
@@ -8,7 +9,11 @@ export default function GlobeScene() {
       <CameraControls />
       <ambientLight />
       <pointLight position={[10, 10, 10]} />
-      <Globe lodLevel={1} latTiles={10} lonTiles={10} />
+      <Detailed distances={[3.2, 3.5, 5]}>
+        <Globe lodLevel={3} latTiles={20} lonTiles={40} />
+        <Globe lodLevel={2} latTiles={10} lonTiles={20} />
+        <Globe lodLevel={1} latTiles={5} lonTiles={10} />
+      </Detailed>
     </Canvas>
   );
 }
